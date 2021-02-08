4c0660fa1b47bc8c5d131cf26e8be0f913d32316
Raphael Rebel <you@example.comraphaelrebel@live.com>
2021-02-08 16:00:46 +0100

    Link naar site toegevoegd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 #Object properties uitvoeren
  
 Een training bij FRO in de module F2M3 DuurzaamHuis
-Link naar live versie:
+#Link naar live versie: http://22503.hosts1.ma-cloud.nl/Periode3/F1M3FRO/les01/index.html
--- dataUitvoer.js
+++ dataUitvoer.js
@@ -1,11 +1,31 @@
 const uitvoer = document.getElementById("uitvoer");
 let dataObject;
 
+const maakDatum = (num) => {
+    // zet de string van de JS datum om in een betere vorm
+    let datum = new Date(num);
+
+    let dagWeek  = datum.getDay();
+    let dagMaand = datum.getDate();
+    let maand    = datum.getMonth();
+    let jaar     = datum.getFullYear();
+    let uren     = datum.getHours();
+    let minuten  = datum.getMinutes();
+
+    return `${dagWeek} </br>
+    ${dagMaand} ${maand} ${jaar} ${uren}:${minuten}`;
+}
 
 const uitvoeren = () => {
     //deze functie voert de data uit in de div met de ID uitvoer
-    uitvoer.innerHTML = `<div>${dataObject[0].tijd} </div>`;
-    uitvoer.innerHTML += `<div>${dataObject[1].tempBuiten} </div>`;
+    let html = "";
+    dataObject.forEach( obj => {
+        html += `<div class="rij">`;
+        html += `<div>${maakDatum(obj.tijd)}</div>`;
+        html += `<div>${obj.tempBuiten}</div>`;
+        html += "</div>";
+    });
+    uitvoer.innerHTML = html
 }
 
 const starten = () => {
